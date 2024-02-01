78256e4dff59cc00bdbb0eec27d076399b20d296
Sailynx Zenefer <siderealism.space@gmail.com>
2024-02-01 16:36:19 +0000

    adds sz&wb&pb page links

--- src/hyperloom/HypAir.jsx
+++ src/hyperloom/HypAir.jsx
@@ -9,11 +9,14 @@ return (
     <p>{"(navigate to ("}</p>
     <nav>
         <HypAirLink endpoint={`${hwPath}siderealism`} linkText={'(siderealism is (what?))'}/>
+        <HypAirLink endpoint={`${hwPath}psytrus-beeshine`} linkText={'(this be (psytrus-beeshine))'}/>
         <HypAirLink endpoint={`${hwPath}psytrus-beeshine/sidereal-sales`} linkText={'(sidereal (sales))'}/>
         <HypAirLink endpoint={`${hwPath}psytrus-beeshine/sidereal-research`} linkText={'(sidereal (research))'}/>
         <HypAirLink endpoint={`${hwPath}psytrus-beeshine/sidereal-tshirts`} linkText={'(sidereal (T.shirts))'}/>
+        <HypAirLink endpoint={`${hwPath}sailynx-zenefer`} linkText={'(this be (sailynx-zenefer))'}/>
         <HypAirLink endpoint={`${hwPath}sailynx-zenefer/sews-gallery`} linkText={'(sews (gallery))'}/>
         <HypAirLink endpoint={`${hwPath}sailynx-zenefer/sees-gallery`} linkText={'(sees (gallery))'}/>
+        <HypAirLink endpoint={`${hwPath}willow-bumble`} linkText={'(this be (willow.bumble))'}/>
         <HypAirLink endpoint={`${hwPath}willow-bumble/sings`} linkText={'(sings of (willow.bumble))'}/>
         <HypAirLink endpoint={`${hwPath}willow-bumble/sounds`} linkText={'(sounds of (willow.bumble))'}/>
         <HypAirLink endpoint={`${hwPath}willow-bumble/cosmic-mumbles`} linkText={'(cosmic mumbles of (willow.bumble))'}/>
